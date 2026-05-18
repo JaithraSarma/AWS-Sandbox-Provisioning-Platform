da9491730c9e3bada8c75faf8ae53065b8ae155d
Jaithra <jaithrasarma@gmail.com>
2026-05-18 15:05:20 +0530

    Expose environment root outputs

---
+++ terraform/environment/outputs.tf
@@ -0,0 +1,20 @@
+output "environment_name" {
+  value = module.environment.environment_name
+}
+
+output "instance_id" {
+  value = module.environment.instance_id
+}
+
+output "instance_public_ip" {
+  value = module.environment.instance_public_ip
+}
+
+output "vpc_id" {
+  value = module.environment.vpc_id
+}
+
+output "metadata_parameter_name" {
+  value = module.environment.metadata_parameter_name
+}
+
